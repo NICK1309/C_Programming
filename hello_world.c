f4e56bfbb79c00c95c33c03659a3f018a14071c0
Nikita Kamalakar Sali <81757315+NICK1309@users.noreply.github.com>
2023-01-15 15:23:26 +0530

    Add files via upload

---
+++ hello_world.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // header file library that lets us work with input and output functions, such as printf() (used in line 4). Header files add functionality to C programs.
+
+int main() {
+  printf("Hello World!");
+  return 0;
+}
