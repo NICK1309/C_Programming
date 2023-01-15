3afdc6eaf4cd652dc4e688aecb0dc1122660eab7
Nikita Kamalakar Sali <81757315+NICK1309@users.noreply.github.com>
2023-01-15 15:49:54 +0530

    Add files via upload

---
+++ variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h> // header file library that lets us work with input and output functions, such as printf() (used in line 4). Header files add functionality to C programs.
+
+int main() {
+// Create variables
+int myNum = 15;            // Integer (whole number)
+float myFloatNum = 5.99;   // Floating point number
+char myLetter = 'D';       // Character
+
+// Print variables
+printf("%d\n", myNum);
+printf("%f\n", myFloatNum);
+printf("%c\n", myLetter);
+
+
+return 0;
+}
