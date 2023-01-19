85a81b222e89f34391bbdaa437a601caaaf84718
Nikita Kamalakar Sali <81757315+NICK1309@users.noreply.github.com>
2023-01-19 14:04:32 +0530

    Add files via upload

---
+++ booleans.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include <stdbool.h>
+int main(){
+// Create boolean variables
+bool isProgrammingFun = true;
+bool isFishTasty = false;
+
+// Return boolean values
+printf("%d", isProgrammingFun);   // Returns 1 (true)
+printf("%d", isFishTasty);        // Returns 0 (false)
+printf("%d", 10 > 9);  // Returns 1 (true) because 10 is greater than 9
+
+
+    return 0;
+}
