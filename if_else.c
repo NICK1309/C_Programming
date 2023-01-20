4e1e64b637f0b0762e0e09c6bc66134465979c44
Nikita Kamalakar Sali <81757315+NICK1309@users.noreply.github.com>
2023-01-20 17:59:28 +0530

    Add files via upload

---
+++ if_else.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+  int x = 20;
+  int y = 18;
+  if (x > y) {
+    printf("x is greater than y");
+int time = 22;
+if (time < 10) {
+  printf("Good morning.");
+} else if (time < 20) {
+  printf("Good day.");
+} else {
+  printf("Good evening.");
+}
+// Outputs "Good evening."
+
+  }  
+  return 0;
+}
